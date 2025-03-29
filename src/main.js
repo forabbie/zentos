00cf6bea3a143094592588fc1e5d1473148507ce
abbie <supermercadoabbie@gmail.com>
2025-03-30 02:01:22 +0800

    Chore: install and configure Primevue

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Lara from '@primeuix/themes/lara'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+  },
+})
 app.use(router)
 
 app.mount('#app')
