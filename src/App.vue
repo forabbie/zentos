fe1bcbdafa0120e84a54b36463e59dec33a66f74
abbie <supermercadoabbie@gmail.com>
2025-04-04 18:04:27 +0800

    Chore: clean up components and styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <AppSidebar />
-    <main class="app-main flex grow flex-col bg-slate-50">
+    <main id="app-main " class="flex grow flex-col bg-slate-50">
       <AppHeader />
       <section class="grow">
         <RouterView />
